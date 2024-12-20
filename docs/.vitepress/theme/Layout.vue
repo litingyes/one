3eade003044cdbef2466e6b99099594ac3d1ecb4
litingyes <luz.liting@gmail.com>
2024-12-20 08:10:51 +0800

    chore(theme): split layout

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import DefaultTheme from 'vitepress/theme'
+
+defineOptions({
+  name: 'DocLayout',
+})
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout />
+</template>
